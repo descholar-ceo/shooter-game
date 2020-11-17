3c6af805c38689606138a243dd176ab58804693e
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 15:44:07 +0200

    Add constructor and key

--- src/entities/entities.js
+++ src/entities/entities.js
@@ -1,3 +1,9 @@
-class Entity {}
+import Phaser from 'phaser';
+
+class Entity extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, key, type) {
+    super(scene, x, y, key);
+  }
+}
 
 export default Entity;
