3defa10d156c41826137ceca58103e1c0757cb03
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 20:16:50 +0200

    Refactor the velocity y

--- src/entities/chaserShip.js
+++ src/entities/chaserShip.js
@@ -1,8 +1,10 @@
+import Phaser from 'phaser';
 import Entity from './entities';
 
 class ChaserShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, 'sprEnemy1', 'ChaserShip');
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
   }
 }
 
--- src/entities/gunShip.js
+++ src/entities/gunShip.js
@@ -1,9 +1,11 @@
+import Phaser from 'phaser';
 import Entity from './entities';
 
 class GunShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, 'sprEnemy0', 'GunShip');
     // this.play('sprEnemy0');
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
   }
 }
 
