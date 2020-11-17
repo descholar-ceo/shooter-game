904eaa3dd500ce5680735dc0870c7d3c7af75142
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 20:25:04 +0200

    Add first enemy :point_right: gun

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Player from '../entities/player';
+import GunShip from '../entities/gunShip';
 import config from '../config/config';
 // import logoImg from '../assets/logo.png';
 
@@ -11,6 +12,18 @@ class GameScene extends Phaser.Scene {
   create() {
     this.player = new Player(this, config.width * 0.5, config.height * 0.5, 'sprPlayer');
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+    this.time.addEvent({
+      delay: 100,
+      callback() {
+        const enemy = new GunShip(this, Phaser.Math.Between(0, config.width), 0);
+        this.enemies.add(enemy);
+      },
+      callbackScope: this,
+      loop: true,
+    });
   }
 
   update() {
