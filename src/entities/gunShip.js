2e6cac75d0f2c1945c09453fb96ac691dfc6667c
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 20:31:59 +0200

    Add enemy laser class

--- src/entities/gunShip.js
+++ src/entities/gunShip.js
@@ -1,11 +1,26 @@
 import Phaser from 'phaser';
 import Entity from './entities';
+import EnemyLaser from './enemyLaser';
 
 class GunShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, 'sprEnemy0', 'GunShip');
     // this.play('sprEnemy0');
     this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.shootTimer = this.scene.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        var laser = new EnemyLaser(
+          this.scene,
+          this.x,
+          this.y
+        );
+        laser.setScale(this.scaleX);
+        this.scene.enemyLasers.add(laser);
+      },
+      callbackScope: this,
+      loop: true
+    });
   }
 }
 
