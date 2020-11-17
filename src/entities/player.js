19cbf7e035e6a4ece994cb03cad166f86412a287
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 15:59:38 +0200

    Add movement functions

--- src/entities/player.js
+++ src/entities/player.js
@@ -3,6 +3,24 @@ import Entity from './entities';
 class Player extends Entity {
   constructor(scene, x, y, key) {
     super(scene, x, y, key, 'Player');
+    this.setData('speed', 200);
+    this.play('sprPlayer');
+  }
+
+  moveUp() {
+    this.body.velocity.y = -this.getData('speed');
+  }
+
+  moveDown() {
+    this.body.velocity.y = this.getData('speed');
+  }
+
+  moveLeft() {
+    this.body.velocity.x = -this.getData('speed');
+  }
+
+  moveRight() {
+    this.body.velocity.x = this.getData('speed');
   }
 }
 
