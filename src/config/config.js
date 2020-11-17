c2198fe29061901e204de341906a9d109ee734e1
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 11:34:33 +0200

    Add physics and pixels

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,15 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
   backgroundColor: 0x000,
   width: 800,
   height: 600,
+  pixelArt: true,
+  roundPixels: true,
 };
