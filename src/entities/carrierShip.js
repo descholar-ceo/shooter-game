d22205d67390b18b29768fc42a3ce7a8f821bc40
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 20:05:32 +0200

    Add carriership content

---
+++ src/entities/carrierShip.js
@@ -0,0 +1,10 @@
+import Entity from './entities';
+
+class CarrierShip extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'sprEnemy2', 'CarrierShip');
+    this.play('sprEnemy2');
+  }
+}
+
+export default CarrierShip;
