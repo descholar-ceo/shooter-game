969a52b7c3d17566154408da4d83548e4752114b
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-17 11:20:05 +0200

    Make webpack config meaningful

--- webpack/production.js
+++ webpack/production.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-const base = require('./base');
+const base = require('./develop');
 
 module.exports = merge(base, {
   mode: 'production',
