50711359031c680b9cd9ba33bad6a25938e41c6c
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-18 10:44:52 +0200

    Add ability to shoot of the player

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,12 +1,16 @@
 import Phaser from 'phaser';
 import Entity from './entities';
 import config from '../config/config';
+import PlayerLaser from './playerLaser';
 
 class Player extends Entity {
   constructor(scene, x, y, key) {
     super(scene, x, y, key, 'Player');
     this.setData('speed', 200);
     // this.play('sprPlayer');
+    this.setData('isShooting', false);
+    this.setData('timerShootDelay', 10);
+    this.setData('timerShootTick', this.getData('timerShootDelay') - 1);
   }
 
   moveUp() {
@@ -29,6 +33,17 @@ class Player extends Entity {
     this.body.setVelocity(0, 0);
     this.x = Phaser.Math.Clamp(this.x, 0, config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, config.height);
+    if (this.getData('isShooting')) {
+      if (this.getData('timerShootTick') < this.getData('timerShootDelay')) {
+        this.setData('timerShootTick', this.getData('timerShootTick') + 1); // every game update, increase timerShootTick by one until we reach the value of timerShootDelay
+      } else { // when the "manual timer" is triggered:
+        const laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+
+        this.scene.sfx.laser.play(); // play the laser sound effect
+        this.setData('timerShootTick', 0);
+      }
+    }
   }
 }
 
