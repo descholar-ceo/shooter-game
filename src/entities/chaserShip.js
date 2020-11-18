1a62b279747ed1650e5f461f5d896a1a3461afa2
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-18 09:53:04 +0200

    Add schasership object to track its moves

--- src/entities/chaserShip.js
+++ src/entities/chaserShip.js
@@ -5,6 +5,11 @@ class ChaserShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, 'sprEnemy1', 'ChaserShip');
     this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.states = {
+      MOVE_DOWN: 'MOVE_DOWN',
+      CHASE: 'CHASE',
+    };
+    this.state = this.states.MOVE_DOWN;
   }
 }
 
