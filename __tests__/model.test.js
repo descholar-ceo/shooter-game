88892d19e809ccdfc59344c5f921aea0f0433891
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-21 21:01:16 +0200

    Add soundon test

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -3,8 +3,13 @@ import Model from '../src/model';
 describe('Test model', () => {
   const model = new Model();
   it('Will setup the musicOn to off', (done) => {
-    model.musicOn = true;
-    expect(model.musicOn).toEqual(true);
+    model.musicOn = false;
+    expect(model.musicOn).toEqual(false);
+    done();
+  });
+  it('Will setup the soundOn to off', (done) => {
+    model.soundOn = false;
+    expect(model.soundOn).toEqual(false);
     done();
   });
 });
