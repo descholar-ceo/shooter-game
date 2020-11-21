4e087ed1d06024dd003fc5e1688061b2ae120721
descholar-ceo <emmanuellamugi@gmail.com>
2020-11-21 16:36:46 +0200

    Finish leaderboard

--- src/scenes/LeaderboardScene.js
+++ src/scenes/LeaderboardScene.js
@@ -10,6 +10,7 @@ export default class LeaderboardScene extends Phaser.Scene {
   // eslint-disable-next-line class-methods-use-this
   preload() {
     retrievePlayerScore();
+    this.topScoredPlayers = JSON.parse(localStorage.getItem('topTenPlayers'));
   }
 
   create() {
@@ -17,5 +18,14 @@ export default class LeaderboardScene extends Phaser.Scene {
       (config.width / 4), (config.height / 2) - 200,
       'Top 10 players', { fontSize: '42px', fill: '#ffffff' },
     );
+    const players = this.topScoredPlayers;
+    for (let index = 0; index < players.length; index += 1) {
+      if (players[index]) {
+        this.add.text(
+          (config.width / 4), (config.height / 2) - 100 + (index * 50),
+          `${index + 1}. ${players[index][0]}: ${players[index][1]}`, { fontSize: '24px', fill: '#ffffff' },
+        );
+      }
+    }
   }
 }
